5ca685e3f5cc4e22864f6b4e4a4fed0f4e709648
Biqi <liubq919@163.com>
2015-04-28 23:49:55 +0800

    Finished calculating the OM
    Finished calculating the OM

--- app/src/main/java/com/example/hilcg/galleryreading2/PicToGray.java
+++ app/src/main/java/com/example/hilcg/galleryreading2/PicToGray.java
@@ -11,6 +11,9 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.liu.common.CommonUtility;
+import com.liu.common.ImageUtil;
+
+import java.util.ArrayList;
 
 /**
  * Created by hilcg on 2015/4/9.
@@ -32,6 +35,7 @@ public class PicToGray extends ActionBarActivity {
         final int height = obmp.getHeight();
         imageView.setDrawingCacheEnabled(false);
         **/
+
         Drawable drawable = getResources().getDrawable(R.drawable.avatar);
         BitmapDrawable bd = (BitmapDrawable)drawable;
         final Bitmap bitmap = bd.getBitmap();
@@ -39,10 +43,134 @@ public class PicToGray extends ActionBarActivity {
         final int width = bitmap.getWidth();
         final int height = bitmap.getHeight();
 
+        int color = bitmap.getPixel(0, 0);
+        final int brightnessValue = ImageUtil.getBrightness(color);
+
+        Bitmap bitMapToGray = CommonUtility.bitmap2Gray(bitmap);
+
+        final ArrayList<Integer> brightVauleList = new ArrayList<>();
+
+        int allPixel = ( width / 3 ) * ( height / 3);
+
+        // Part A
+        int aPart = 0;
+        for ( int x = 0 ; x <= width / 3 ; x ++ )
+        {
+            for ( int y = 0 ; y <= height / 3; y++ )
+            {
+                aPart = aPart + ImageUtil.getBrightness(bitMapToGray.getPixel(x,y));
+            }
+        }
+        brightVauleList.add( aPart / allPixel);
+
+        final int a = aPart;
+
+        // Part B
+        int bPart = 0;
+        for ( int x = width / 3; x <= ( width * 2 ) / 3; x ++ )
+        {
+            for ( int y = 0 ; y <= height / 3; y++ )
+            {
+                bPart = bPart + ImageUtil.getBrightness(bitMapToGray.getPixel(x,y));
+            }
+        }
+        brightVauleList.add( bPart / allPixel);
+
+        final int b = bPart;
+
+        // Part C
+        int cPart = 0;
+        for ( int x = ( width * 2 ) / 3; x < width ; x ++ )
+        {
+            for ( int y = 0 ; y <= height / 3; y++ )
+            {
+                cPart = cPart + ImageUtil.getBrightness(bitMapToGray.getPixel(x,y));
+            }
+        }
+
+        brightVauleList.add( cPart / allPixel);
+
+        // Part D
+        int dPart = 0;
+        for ( int x = 0; x <= width / 3 ; x ++ )
+        {
+            for ( int y = height / 3 ; y <= ( height / 3 ) * 2; y++ )
+            {
+                dPart = dPart + ImageUtil.getBrightness(bitMapToGray.getPixel(x,y));
+            }
+        }
+
+        brightVauleList.add( dPart / allPixel);
+
+        // Part E
+        int ePart = 0;
+        for ( int x = width / 3; x <= ( width * 2 ) / 3; x ++ )
+        {
+            for ( int y = height / 3 ; y <= ( height / 3 ) * 2; y++ )
+            {
+                ePart = ePart + ImageUtil.getBrightness(bitMapToGray.getPixel(x,y));
+            }
+        }
+
+        brightVauleList.add( ePart / allPixel);
+
+        // Part F
+        int fPart = 0;
+        for ( int x = ( width * 2 ) / 3; x < width ; x ++ )
+        {
+            for ( int y = height / 3 ; y <= ( height / 3 ) * 2; y++ )
+            {
+                fPart = fPart + ImageUtil.getBrightness(bitMapToGray.getPixel(x,y));
+            }
+        }
+
+        brightVauleList.add( fPart / allPixel);
+
+        // Part G
+        int gPart = 0;
+        for ( int x = 0 ; x <= width / 3 ; x ++ )
+        {
+            for ( int y = ( height / 3 ) * 2; y < height ; y++ )
+            {
+                gPart = gPart + ImageUtil.getBrightness(bitMapToGray.getPixel(x,y));
+            }
+        }
+
+        brightVauleList.add( gPart / allPixel);
+
+        // Part H
+        int hPart = 0;
+        for ( int x = width / 3; x <= ( width * 2 ) / 3; x ++ )
+        {
+            for ( int y = ( height / 3 ) * 2; y < height ; y++ )
+            {
+                hPart = hPart + ImageUtil.getBrightness(bitMapToGray.getPixel(x,y));
+            }
+        }
+
+        brightVauleList.add( hPart / allPixel);
+
+        // Part I
+        int iPart = 0;
+        for ( int x = ( width * 2 ) / 3; x < width ; x ++ )
+        {
+            for ( int y = ( height / 3 ) * 2; y < height ; y++ )
+            {
+                iPart = iPart + ImageUtil.getBrightness(bitMapToGray.getPixel(x,y));
+            }
+        }
+
+        brightVauleList.add( iPart / allPixel );
+
         btnStartToGray.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(PicToGray.this, "图片属性: width: " + width + "; height: " + height + "GetPixel():" + bitmap.getPixel(100, 100), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(PicToGray.this, "图片属性: width: " + width + "; height: " + height + "a2:" + a2, Toast.LENGTH_SHORT).show();
+
+//                Toast.makeText(PicToGray.this, "a:" + a + ";b:" + b, Toast.LENGTH_LONG).show();
+
+                Toast.makeText(PicToGray.this, "brightVauleList:" + brightVauleList, Toast.LENGTH_LONG).show();
+
                 imageView.setImageBitmap(CommonUtility.bitmap2Gray(bitmap));
             }
         });
