c0d3fd5ca3d6955620223a66f7bc53ba6e66d011
liubq919 <liubq919@163.com>
2015-04-29 14:05:18 +0800

    对OM值的排序

--- app/src/main/java/com/liu/common/CommonUtility.java
+++ app/src/main/java/com/liu/common/CommonUtility.java
@@ -6,6 +6,10 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by hilcg on 2015/4/9.
  */
@@ -27,5 +31,49 @@ public class CommonUtility {
         c.drawBitmap(bmSrc, 0, 0, paint);
 
         return bmpGray;
+    }
+
+    public static List<Integer> sortIndexOfList( ArrayList<Integer> list)
+    {
+        public static List<Integer> sortIndexOfList( ArrayList<Integer> list)
+        {
+            if ( 0 == list.size())
+            {
+                return null;
+            }
+
+            ArrayList<Integer> unSortedList = new ArrayList<Integer>();
+            unSortedList = (ArrayList<Integer>)list.clone();
+
+            Collections.sort(list);
+            Collections.reverse(list);
+
+            System.out.println(list);
+            System.out.println(unSortedList);
+
+            ArrayList<Integer> sortedIndexOfArrayList = new ArrayList<Integer>();
+
+//
+//        for( int i = 0; i < unSortedList.size(); i ++ )
+//        {
+//
+//            System.out.println(unSortedList.get(i) + ", ");
+//
+//            for ( int j = 0; j < list.size(); j ++ )
+//            {
+//
+//                System.out.print(list.get(j) + ", ");
+//
+//                if ( list.get(j) == unSortedList.get(i) )
+//                {
+//                    System.out.println(list.get(j));
+//                }
+//            }
+//            System.out.println();
+//
+//        }
+
+            return sortedIndexOfArrayList;
+
     }
 }
